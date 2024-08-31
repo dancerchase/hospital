7ff8c4277614f1d81b6e6d66a64ac82f23a4873d
dancer <3678vg1@mail.ru>
2024-08-31 08:12:13 +0500

    Отделение методов для выполнения команд в отдельный класс (надеюсь реализация правилная)

--- hospital.py
+++ hospital.py
@@ -1,7 +1,8 @@
 from patient_data import PatientData
 from user_output_commands import UserOutputCommands
 from user_input_commands import UserInputCommands
-from errors import InputCommandError, IDNotIntOrNegativeError, IDNotExistError, MinStatusError, MaxStatusError
+from actions_for_commands import ActionsForCommands
+from errors import InputCommandError, IDNotIntOrNegativeError, IDNotExistError
 
 
 class Hospital:
@@ -12,90 +13,29 @@ class Hospital:
         self._patient_data = PatientData()
         self._user_input = UserInputCommands()
         self._user_output = UserOutputCommands()
+        self._actions = ActionsForCommands()
 
     def _performing_an_action_based_on_a_command(self, command: str):
         """Выполняет действие в зависимости от команды"""
 
         if command in ['узнать статус пациента', 'get status']:
             patient_id = self._get_user_id_and_checking()
-            self._print_patient_status(patient_id)
+            self._actions.print_patient_status(patient_id)
 
         elif command in ['повысить статус пациента', 'status up']:
             patient_id = self._get_user_id_and_checking()
-            self._up_status_for_patient(patient_id)
+            self._actions.up_status_for_patient(patient_id)
 
         elif command in ['понизить статус пациента', 'status down']:
             patient_id = self._get_user_id_and_checking()
-            self._down_status_for_patient(patient_id)
+            self._actions.down_status_for_patient(patient_id)
 
         elif command in ['выписать пациента', 'discharge']:
             patient_id = self._get_user_id_and_checking()
-            self._discharge_patient(patient_id)
+            self._actions.discharge_patient(patient_id)
 
         elif command in ['рассчитать статистику', 'calculate statistics']:
-            self._print_hospital_statistic()
-
-    def _print_patient_status(self, patient_id: int):
-        """Печать статуса пациента"""
-        try:
-            print(f'Статус пациента: "{self._patient_data.get_patient_status_text(patient_id)}"')
-        except IDNotExistError:
-            self._user_output.error_patient_id_not_exist()
-
-        except TypeError:
-            pass
-
-    def _up_status_for_patient(self, patient_id: int):
-        """Повышение статуса пациента"""
-        try:
-            self._patient_data.up_status_for_patient(patient_id)
-            new_status = self._patient_data.get_patient_status_text(patient_id)
-            self._user_output.new_status(new_status)
-
-        except IDNotExistError:
-            self._user_output.error_patient_id_not_exist()
-
-        except MaxStatusError:
-            if self._user_input.hospital_discharge_offer() == 'да':
-                self._patient_data.patient_discharge(patient_id)
-                self._user_output.discharge()
-            else:
-                self._user_output.refusal_of_discharge()
-
-        except TypeError:
-            pass
-
-    def _down_status_for_patient(self, patient_id: int):
-        """Понижение статуса пациента"""
-        try:
-            self._patient_data.down_status_for_patient(patient_id)
-            new_status = self._patient_data.get_patient_status_text(patient_id)
-            self._user_output.new_status(new_status)
-
-        except IDNotExistError:
-            self._user_output.error_patient_id_not_exist()
-
-        except MinStatusError:
-            self._user_output.not_approval_discharge()
-
-        except TypeError:
-            pass
-
-    def _discharge_patient(self, patient_id: int):
-        """Выписка пациента"""
-        try:
-            self._patient_data.patient_discharge(patient_id)
-            self._user_output.discharge()
-        except IDNotExistError:
-            self._user_output.error_patient_id_not_exist()
-
-        except TypeError:
-            pass
-
-    def _print_hospital_statistic(self):
-        """Печать статистики больницы"""
-        statistic = self._patient_data.get_statistic_patients()
-        self._user_output.statistic_patients(statistic)
+            self._actions.print_hospital_statistic()
 
     def _check_command_in_list_available(self, command: str):
         """Проверяет правильность ввода команды"""
