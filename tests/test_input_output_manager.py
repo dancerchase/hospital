1f1a1a4b87611abb0011a50f656a70a0b226779d
dancer <3678vg1@mail.ru>
2024-09-15 17:52:01 +0500

    тесты _convert_patient_id_from_str_to_positive_int

--- tests/test_input_output_manager.py
+++ tests/test_input_output_manager.py
@@ -1,21 +1,16 @@
-import unittest
+import pytest
 from input_output_manager import InputOutputManager
 from errors import PatientIDNotIntOrNegativeError
 
 
-class TestInputOutputManager(unittest.TestCase):
-    def setUp(self):
-        self.input_output_manager = InputOutputManager()
+class TestInputOutputManager:
 
     def test_convert_patient_id_from_str_to_positive_int(self):
-        self.assertEqual(InputOutputManager.convert_patient_id_from_str_to_positive_int('1'), 1)
-        self.assertEqual(InputOutputManager.convert_patient_id_from_str_to_positive_int('200'), 200)
+        assert InputOutputManager._convert_patient_id_from_str_to_positive_int('56') == 56
 
-    def test_convert_patient_id_from_str_to_positive_int_invalid(self):
-        with self.assertRaises(PatientIDNotIntOrNegativeError):
-            InputOutputManager.convert_patient_id_from_str_to_positive_int('0')
-            InputOutputManager.convert_patient_id_from_str_to_positive_int('-1')
-            InputOutputManager.convert_patient_id_from_str_to_positive_int('asd')
-            InputOutputManager.convert_patient_id_from_str_to_positive_int('')
-            InputOutputManager.convert_patient_id_from_str_to_positive_int(' ')
+    @pytest.mark.parametrize('invalid_data', ['0', '-1', 'asd', '', ' '])
+    def test_convert_patient_id_from_str_to_positive_int_invalid_data(self, invalid_data):
+        with pytest.raises(PatientIDNotIntOrNegativeError):
+            InputOutputManager._convert_patient_id_from_str_to_positive_int(invalid_data)
+
 
