14124824a49aeb308152e6183de646e611db76de
dancer <3678vg1@mail.ru>
2024-09-27 08:57:30 +0500

    тест на добавление нового пациента

--- tests/test_actions_for_commands.py
+++ tests/test_actions_for_commands.py
@@ -229,3 +229,13 @@ class TestActionsForCommands:
 
             input_output_manager.send_message_hospital_statistics_text.assert_called_once_with(
                 {'Готов к выписке': 2, 'Тяжело болен': 1, 'Слегка болен': 1}, 4)
+    class TestAddNewPatient:
+        def test_add_new_patient(self):
+            input_output_manager = MagicMock()
+            hospital = Hospital(patients=[1, 2, None], statuses=base_statuses)
+            actions_for_commands = ActionsForCommands(input_output_manager, hospital)
+            input_output_manager.get_new_patient_status.return_value = "Готов к выписке"
+            actions_for_commands.add_new_patient()
+
+            assert hospital._patients == [1, 2, None, 3]
+            input_output_manager.send_message_patient_added.assert_called_once_with(4)
