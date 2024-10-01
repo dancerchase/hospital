9958bfcdf01e20efc4aab2216f6d6199dac69176
dancer <3678vg1@mail.ru>
2024-10-01 07:40:44 +0500

    передача статусов по умолчанию для запуска программы

--- main.py
+++ main.py
@@ -4,9 +4,11 @@ from actions_for_commands import ActionsForCommands
 from hospital import Hospital
 from input_output.console import Console
 
+base_statuses = {0: "Тяжело болен", 1: "Болен", 2: "Слегка болен", 3: "Готов к выписке"}
+
 input_output_manager = InputOutputManager(Console())
 
-hospital = Hospital()
+hospital = Hospital(statuses=base_statuses)
 
 actions_for_commands = ActionsForCommands(input_output_manager, hospital)
 
